3687b76d217b87ba7cff9dab0eda300dbe7e23b5
stuartErnstsen <stuarternstsen@gmail.com>
2020-12-04 12:58:25 -0700

    Completed all assessment sets for Week 1

--- javascript-1/practice-js-1.js
+++ javascript-1/practice-js-1.js
@@ -2,17 +2,17 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+const myName = "Stuart Ernstsen"
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+const faveNum = 108
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+const lovesCode = true
 //////////////////PROBLEM 4////////////////////
 /*
   Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. 
@@ -20,7 +20,9 @@
 */
 
 //CODE HERE
-
+function sum(num1, num2) {
+  return num1 + num2;
+}
 //////////////////PROBLEM 5////////////////////
 /*
   Create a function called 'trueOrFalse' that takes in a single parameter called 'data'. 
@@ -30,7 +32,12 @@
 */
 
 //CODE HERE
-
+function trueOrFalse(data) {
+  if (data) {
+    return true
+  }
+  return false
+}
 //////////////////PROBLEM 6////////////////////
 /*
   Create a function called 'oddChecker' that takes in one parameter, 'num'. 
@@ -40,7 +47,9 @@
 */
 
 //CODE HERE
-
+function oddChecker(num) {
+  return num % 2 === 0 ? "the number is even" : "the number is odd"
+}
 //////////////////PROBLEM 7////////////////////
 /*
   Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'. 
@@ -49,7 +58,9 @@
 */
 
 //CODE HERE
-
+function iLove(name, love) {
+  return `${name} loves ${love}`
+}
 
 //////////////////PROBLEM 8////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
@@ -57,12 +68,12 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
-
+const colorCopy = faveColors.slice();
 //////////////////PROBLEM 9////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+colorCopy.push("blue")
 //////////////////PROBLEM 10////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -72,7 +83,7 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+middleNums = numbers.slice(1, numbers.length - 1)
 //////////////////PROBLEM 11////////////////////
 /*
   Create an object called 'me' that has the following keys: 
@@ -88,7 +99,14 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 //CODE HERE
-
+const me = {
+  firstName: "Stuart",
+  state: 'UT',
+  age: 30,
+  greeter: function () {
+    return `Hello! My name is ${this.firstName} and I live in ${this.state}`
+  }
+}
 //////////////////PROBLEM 12////////////////////
 /* 
   Create a function called 'bigOrSmall' that takes in one parameter, 'arr', 
@@ -103,7 +121,19 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 // CODE HERE
+function bigOrSmall(arr) {
+  const answers = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 100) {
+      answers.push("big")
+    }
+    else {
+      answers.push("small")
+    }
+  }
+  return answers;
 
+}
 //////////////////PROBLEM 13////////////////////
 /* 
   Create a function called 'arrayReverser' that takes in one parameter, 'arr'. 
@@ -115,7 +145,13 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 //CODE HERE
-
+function arrayReverser(arr) {
+  const reversed = []
+  for (let i = 0; i < arr.length; i++) {
+    reversed.unshift(arr[i])
+  }
+  return reversed
+}
 //////////////////PROBLEM 14////////////////////
 
 let global = 'cool string'
@@ -140,48 +176,57 @@ function secondFunction() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ['global', 'inner', 'outer', 'functional']
+let globalScope = ['global']
 
 //This array should contain the variable names (as strings) accessible in the firstFunction function.
-let firstFunctionScope = ['global', 'inner', 'outer', 'functional']
+let firstFunctionScope = ['global', 'outer']
 
 //This array should contain the variable names (as strings) accessible in the innerFunction function.
-let innerFunctionScope = ['global', 'inner', 'outer', 'functional']
+let innerFunctionScope = ['global', 'inner', 'outer']
 
 //This array should contain the variable names (as strings) accessible in the secondFunction function.
-let secondFunctionScope = ['global', 'inner', 'outer', 'functional']
+let secondFunctionScope = ['global', 'functional']
 
 
 //////////////////PROBLEM 15////////////////////
-/* 
-  Write a function called firstItem that takes in two parameters: an array and a callback.  
+/*
+  Write a function called firstItem that takes in two parameters: an array and a callback.
   Invoke the callback passing the first item in the array as an argument.
 */
 
 //CODE HERE
-
+function firstItem(arr, cb) {
+  cb(arr[0])
+}
 //////////////////PROBLEM 16////////////////////
-/* 
-  Write a function called isItBob that takes in two arguments: an object and a callback function. 
-  Check if the object's name property is equal to 'Bob'.  
-  If it is, invoke the callback passing in true as an argument.  
+/*
+  Write a function called isItBob that takes in two arguments: an object and a callback function.
+  Check if the object's name property is equal to 'Bob'.
+  If it is, invoke the callback passing in true as an argument.
   Otherwise, invoke the callback passing in false.
 */
 
 //CODE HERE
-
+function isItBob(obj, cb) {
+  return obj.name === "Bob" ? cb(true) : cb(false)
+}
 //////////////////PROBLEM 17////////////////////
 /*
-  Write a function called giveMeDoubles that takes in two arguments: an array of numbers and a callback.  
+  Write a function called giveMeDoubles that takes in two arguments: an array of numbers and a callback.
   Use a for loop to double all numbers in the array.
   Then invoke the callback, passing in the doubled array.
 */
 
 //CODE HERE
-
+function giveMeDoubles(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] *= 2
+  }
+  cb(arr)
+}
 //////////////////PROBLEM 18////////////////////
 /*
-  Write a function called carFactory that takes in three parameters: a make, model, and year.  
+  Write a function called carFactory that takes in three parameters: a make, model, and year.
   When the function is invoked:
     - a string will be sent in for make
     - a string will be sent in for model
@@ -197,7 +242,7 @@ let secondFunctionScope = ['global', 'inner', 'outer', 'functional']
     carFactory('toyota', 'camry', 2020)
   should return an object that looks like this:
   {
-    make: 'toyota', 
+    make: 'toyota',
     model: 'camry',
     year: 2020,
     isNew: true
@@ -205,3 +250,8 @@ let secondFunctionScope = ['global', 'inner', 'outer', 'functional']
 */
 
 //CODE HERE
+function carFactory(make, model, year) {
+  const obj = { make, model, year };
+  obj.isNew = obj.year > 2018 ? true : false;
+  return obj;
+}
--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -10,55 +10,67 @@
 */
 
 //CODE HERE
-
+const gitDefinition = "Git is a version control system that allows you to create repositories in which you can track current/previous files and changes made to throughout the life of the project"
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable called 'gitHubDefinition'.  
+    Create a variable called 'gitHubDefinition'.
     It should be a string containing your best definition of what GitHub is.
 */
 
 //CODE HERE
-
+gitHubDefinition = "GitHub is a user interface website that utilized a GUI to view and interact with repo's created and maintained with Git"
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
-    For the next several problems you will be creating objects containing information about different git 
-    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
-    will be a string with a description of what that git command does.  The 'code' property should be a 
+    For the next several problems you will be creating objects containing information about different git
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property
+    will be a string with a description of what that git command does.  The 'code' property should be a
     string of the actual command (what you type into your terminal).
 */
 
 //////////////////PROBLEM 3////////////////////
 /*
-    Create an object called 'init' with 'description' and 'code' properties 
+    Create an object called 'init' with 'description' and 'code' properties
     following the guidelines above to describe the init command.
 */
 
 //CODE HERE
-
+const init = {
+    description: 'Creates and initialized new repository',
+    code: 'git init'
+}
 //////////////////PROBLEM 4////////////////////
 /*
-    Create an object called 'clone' with 'description' and 'code' properties 
+    Create an object called 'clone' with 'description' and 'code' properties
     following the guidelines above to describe the clone command.
 */
 
 //CODE HERE
-
+const clone = {
+    description: 'Creates an exact copy of an existing Git repository',
+    code: 'git clone /path-to-Git-repo-to-be-copied'
+}
 //////////////////PROBLEM 5////////////////////
 /*
-    Create an object called 'status' with 'description' and 'code' properties 
+    Create an object called 'status' with 'description' and 'code' properties
     following the guidelines above to describe the status command.
 */
 
 //CODE HERE
-
+const status = {
+    description: 'Displays a summary of current working Git repo and what changes, if any, have been made to the branch.',
+    code: 'git status'
+}
 //////////////////PROBLEM 6////////////////////
 /*
-    Create an object called 'add' with 'description' and 'code' properties 
+    Create an object called 'add' with 'description' and 'code' properties
     following the guidelines above to describe the add command.
 */
 
 //CODE HERE
-
+const add = {
+    description: 'Takes files that have been edited within working git repo and will "Stage" them to be commited and pushed to repo',
+    code: 'git add nameOfFile.js'
+}
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +78,10 @@
 */
 
 //CODE HERE
-
+const commit = {
+    description: 'Takes "Staged" items and creates a unique revision/id attached to the current content/changes to files in the repo',
+    code: 'git commit -m "Comment of what changes were made in this commit/revision'
+}
 //////////////////PROBLEM 8////////////////////
 /*
     Create an object called 'addRemote' with 'description' and 'code' properties
@@ -74,11 +89,18 @@
 */
 
 //CODE HERE
-
+const addRemote = {
+    description: 'Creates a connection to a specific Git repository that will allow you to push revisions to',
+    code: 'git remote add origin'
+}
 //////////////////PROBLEM 9////////////////////
 /*
-    Create an object called 'push' with 'description' and 'code' properties 
+    Create an object called 'push' with 'description' and 'code' properties
     following the guidelines above to describe the push command.
 */
 
 //CODE HERE
+const push = {
+    description: 'The final step that takes commited revisions and updates the Git repo with those changes',
+    code: 'git push'
+}
